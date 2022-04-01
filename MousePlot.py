d3a7df67907ef59fae7237eb80953d6200b5e734
Stytch <finouxnicolas@gmail.com>
2022-04-01 23:22:34 +0200

    [mousePlot][Utils]Mapping function

--- MousePlot.py
+++ MousePlot.py
@@ -1,15 +1,14 @@
-import math
-import numpy as np
-
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
-
+from Utils import *
 pygame.init()
 
 screen_width = 1000
 screen_height = 800
+ortho_width = 640
+ortho_height = 480
 
 screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
 pygame.display.set_caption('OpenGL in Python')
@@ -18,7 +17,7 @@ pygame.display.set_caption('OpenGL in Python')
 def init_ortho():
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluOrtho2D(0, 1000, 800, 0)
+    gluOrtho2D(0, 640, 0, 480)
 
 
 def plot_point(my_points):
@@ -37,7 +36,9 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
         elif event.type == MOUSEBUTTONDOWN:
-            points.append(pygame.mouse.get_pos())
+            p = pygame.mouse.get_pos()
+            points.append((map_value(0, screen_width, 0, ortho_width, p[0]),
+                           map_value(0, screen_height, ortho_height, 0, p[1])))
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glMatrixMode(GL_MODELVIEW)
