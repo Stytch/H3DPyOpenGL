a4a7a93e845f4d59db8736bffce76909f8f4c6bd
Stytch <finouxnicolas@gmail.com>
2022-04-01 23:29:05 +0200

    [Utils]rename arg

--- Utils.py
+++ Utils.py
@@ -1,4 +1,5 @@
-def map_value(current_min, current_max, new_min, new_max, value):
+def map_value(current_min, current_max, new_min, new_max, current_value):
+    # print(current_value)
     current_range = current_max - current_min
     new_range = new_max - new_min
-    return new_min + new_range * ((value - current_min)/current_range)
+    return new_min + new_range * ((current_value - current_min) / current_range)
